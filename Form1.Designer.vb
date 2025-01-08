334ba35f0d63e8854dcefebce8bc88e7c7b18393
oswaldojmp <oswaldojesusmarinpages@gmail.com>
2025-01-08 05:23:42 -0400

    Agregando Archivos al Repositorio
    Se agregaron archivos al repositorio local git

---
+++ Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMostrarTemp = New System.Windows.Forms.Button()
+        Me.btnItrTemp = New System.Windows.Forms.Button()
+        Me.txtTemp = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnMostrarTemp
+        '
+        Me.btnMostrarTemp.BackColor = System.Drawing.Color.Maroon
+        Me.btnMostrarTemp.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMostrarTemp.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnMostrarTemp.Location = New System.Drawing.Point(183, 327)
+        Me.btnMostrarTemp.Name = "btnMostrarTemp"
+        Me.btnMostrarTemp.Size = New System.Drawing.Size(165, 54)
+        Me.btnMostrarTemp.TabIndex = 5
+        Me.btnMostrarTemp.Text = "Mostrar Temperaturas"
+        Me.btnMostrarTemp.UseVisualStyleBackColor = False
+        '
+        'btnItrTemp
+        '
+        Me.btnItrTemp.BackColor = System.Drawing.Color.Maroon
+        Me.btnItrTemp.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnItrTemp.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnItrTemp.Location = New System.Drawing.Point(12, 327)
+        Me.btnItrTemp.Name = "btnItrTemp"
+        Me.btnItrTemp.Size = New System.Drawing.Size(165, 54)
+        Me.btnItrTemp.TabIndex = 4
+        Me.btnItrTemp.Text = "Introducir Temperaturas"
+        Me.btnItrTemp.UseVisualStyleBackColor = False
+        '
+        'txtTemp
+        '
+        Me.txtTemp.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtTemp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtTemp.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTemp.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.txtTemp.Location = New System.Drawing.Point(12, 12)
+        Me.txtTemp.Multiline = True
+        Me.txtTemp.Name = "txtTemp"
+        Me.txtTemp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtTemp.Size = New System.Drawing.Size(336, 299)
+        Me.txtTemp.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(361, 394)
+        Me.Controls.Add(Me.btnMostrarTemp)
+        Me.Controls.Add(Me.btnItrTemp)
+        Me.Controls.Add(Me.txtTemp)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Temperaturas de Arreglo Dinamico"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMostrarTemp As Button
+    Friend WithEvents btnItrTemp As Button
+    Friend WithEvents txtTemp As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Dim Temperatura() As Single 'Arreglo Dinamico
+    Dim Dias As Integer
+
+    Private Sub btnItrTemp_Click(sender As Object, e As EventArgs) Handles btnItrTemp.Click
+        Dim Prompt, Title As String
+        Dim i As Short
+        Prompt = "Escriba la temperatura mas alta del dia"
+
+        Dias = InputBox("Cuantos dias?", "Crear Arreglo")
+        If Dias > 0 Then ReDim Temperatura(Dias - 1)
+
+        For i = 0 To UBound(Temperatura) 'Ubond: Funcion que devuelve el indice superior de un Arreglo
+            Title = "Dia " & (i + 1)
+            Temperatura(i) = InputBox(Prompt, Title)
+        Next
+        'la palabra reservada Preserve conserva los datos del arreglo aunque cambien sus dimensiones
+        'Dim filosofos(200)
+        'filosofos(200) = "Alfonso Reyes"
+        'Redim Preserve filosofos(300)
+    End Sub
+
+    Private Sub btnMostrarTemp_Click(sender As Object, e As EventArgs) Handles btnMostrarTemp.Click
+
+        Dim Resultado As String
+        Dim i As Short
+        Dim Total As Single = 0
+        Resultado = "Temperaturas altas:" & vbCrLf & vbCrLf
+
+        For i = 0 To UBound(Temperatura) 'Ubond: Funcion que devuelve el indice superior de un Arreglo
+            Resultado = Resultado & "Dia " & (i + 1) & vbTab & Temperatura(i) & vbCrLf
+            Total = Total + Temperatura(i)
+        Next
+        'vbCrLf: Retorno de Carro./n
+        'vbTab: Tabulador
+        Resultado = Resultado & vbCrLf & "Temperatura Promedio: " & Format(Total / Dias, "0.0")
+        txtTemp.Text = Resultado
+    End Sub
+End Class
